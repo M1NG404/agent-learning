e1b7b824894af4a8df24d00cf7002740adebf4b3
M1NG404 <2995195213@qq.com>
2026-09-02 15:13:09 +0800

    refactor: inject shared memory store

--- tools/registry.py
+++ tools/registry.py
@@ -1,29 +1,22 @@
 
 # 给 Runtime 用的 Tool 注册表
 
-from functools import partial
-
-from memory.store import MemoryStore
 from models.order import OrderArgs
 from tools.memory_tools import save_memory
 from tools.order_tools import cancel_order, get_order
 from models.memory_model import SaveMemoryArgs  
 
-def create_tool_registry(memory_store: MemoryStore):
-    return {
-        "get_order": {
-                "function": get_order,
-                "args_model": OrderArgs
-            },
-            "cancel_order": {
-                "function": cancel_order,
-                "args_model": OrderArgs
-            },
-            "save_memory": {
-                "function": partial(
-                    save_memory, 
-                    store=memory_store
-                ),   
-                "args_model": SaveMemoryArgs  # 这里可以放置实际的保存内存的参数模型
-            }
+tool_registry = {
+    "get_order": {
+        "function": get_order,
+        "args_model": OrderArgs
+    },
+    "cancel_order": {
+        "function": cancel_order,
+        "args_model": OrderArgs
+    },
+    "save_memory": {
+        "function": save_memory,  # 这里可以放置实际的保存内存的函数
+        "args_model": SaveMemoryArgs  # 这里可以放置实际的保存内存的参数模型
     }
+}
